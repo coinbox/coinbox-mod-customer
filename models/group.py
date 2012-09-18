dd6996676ed20622fee557fb34798673415a70b4
jadkik <jadkik94@gmail.com>
2012-09-18 22:39:40 +0300

    Initial commit

---
+++ models/group.py
@@ -0,0 +1,26 @@
+import cbpos
+
+import cbpos.database
+import cbpos.mod.base.models.common as common
+
+from sqlalchemy import func, Table, Column, Integer, String, Float, Boolean, MetaData, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method, Comparator
+
+class CustomerGroup(cbpos.database.Base, common.Item):
+    __tablename__ = 'customergroups'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    comment = Column(String(255), nullable=True)
+
+    @hybrid_property
+    def display(self):
+        return self.name
+    
+    @display.expression
+    def display(self):
+        return self.name
+
+    def __repr__(self):
+        return "<CustomerGroup %s>" % (self.name)
